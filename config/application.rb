e05cf986b9dcb75ee8194ee6dfd7a0562dad2119
Fred Wu <ifredwu@gmail.com>
2011-05-24 08:11:30 +1000

    'password_confirmation' needs to be filtered as well.

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module AngelNest
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable the asset pipeline
     config.assets.enabled = true
