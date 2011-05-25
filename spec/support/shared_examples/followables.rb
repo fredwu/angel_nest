02551727fdce455ba5279417cb03dc4bf78736ac
Fred Wu <ifredwu@gmail.com>
2011-05-25 18:14:59 +1000

    More comprehensive tests for follow/unfollow.

--- spec/support/shared_examples/followables.rb
+++ spec/support/shared_examples/followables.rb
@@ -1,6 +1,7 @@
 shared_examples "followables" do
-  subject      { User.make! }
-  let(:target) { described_class.make! }
+  subject       { User.make! }
+  let(:target)  { described_class.make! }
+  let(:target2) { described_class.make! }
 
   it "does not follow a target by default" do
     subject.is_following?(target).should == false
@@ -12,17 +13,32 @@ shared_examples "followables" do
   it "follows a target" do
     subject.follow(target)
     subject.is_following?(target).should == true
+    subject.is_following?(target2).should == false
     target.is_followed_by?(subject).should == true
+    target2.is_followed_by?(subject).should == false
     subject.followings.count.should == 1
     target.followers.count.should == 1
+    target2.followers.count.should == 0
+    subject.follow(target2)
+    subject.followings.count.should == 2
+    target.followers.count.should == 1
+    target2.followers.count.should == 1
   end
 
   it "unfollows a target" do
     subject.follow(target)
+    subject.follow(target2)
     subject.unfollow(target)
     subject.is_following?(target).should == false
+    subject.is_following?(target2).should == true
     target.is_followed_by?(subject).should == false
+    target2.is_followed_by?(subject).should == true
+    subject.followings.count.should == 1
+    target.followers.count.should == 0
+    target2.followers.count.should == 1
+    subject.unfollow(target2)
     subject.followings.count.should == 0
     target.followers.count.should == 0
+    target2.followers.count.should == 0
   end
 end
