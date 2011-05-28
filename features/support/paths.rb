09ca35f87351e1f5d77a70810cadc23edb55b63f
Fred Wu <ifredwu@gmail.com>
2011-05-28 20:40:12 +1000

    Fixed Pickle for Cucumber.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,6 +11,20 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
+    # the following are examples using path_to_pickle
+
+    when /^#{capture_model}(?:'s)? page$/                           # eg. the forum's page
+      path_to_pickle $1
+
+    when /^#{capture_model}(?:'s)? #{capture_model}(?:'s)? page$/   # eg. the forum's post's page
+      path_to_pickle $1, $2
+
+    when /^#{capture_model}(?:'s)? #{capture_model}'s (.+?) page$/  # eg. the forum's post's comments page
+      path_to_pickle $1, $2, :extra => $3                           #  or the forum's post's edit page
+
+    when /^#{capture_model}(?:'s)? (.+?) page$/                     # eg. the forum's posts page
+      path_to_pickle $1, :extra => $2                               #  or the forum's edit page
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
--- features/support/pickle.rb
+++ features/support/pickle.rb
@@ -16,8 +16,11 @@
 # You may also need to add gem dependencies on your factory of choice in <tt>config/environments/cucumber.rb</tt>
 
 require 'pickle/world'
-
-Pickle.configure do |config|
-  config.adapters = [:machinist, :active_record]
-  config.map 'I', 'myself', 'me', 'my', :to => 'user: "me"'
-end
+# Example of configuring pickle:
+#
+# Pickle.configure do |config|
+#   config.adapters = [:machinist]
+#   config.map 'I', 'myself', 'me', 'my', :to => 'user: "me"'
+# end
+require 'pickle/path/world'
+require 'pickle/email/world'
