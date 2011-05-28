683288bfabd3becd4cbd28ee07e6db15150033ef
Fred Wu <ifredwu@gmail.com>
2011-05-29 01:18:58 +1000

    Added a few geo location detector helper methods.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,33 @@
 module UsersHelper
-  def find_geo_location(remote_ip = nil)
-    GeoLocation.find(remote_ip || request.remote_ip)
+  def find_geo_location
+    GeoLocation.find(request.remote_ip)
+  end
+
+  def detected_country
+    find_geo_location[:country]
+  end
+
+  def detected_country_code
+    find_geo_location[:country_code]
+  end
+
+  def detected_city_and_region
+    if detected_city && detected_region
+      "#{detected_city}, #{detected_region}"
+    else
+      "#{detected_city}#{detected_region}"
+    end
+  end
+
+  def detected_city
+    find_geo_location[:city]
+  end
+
+  def detected_region
+    find_geo_location[:region]
+  end
+
+  def detected_timezone
+    find_geo_location[:timezone]
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,20 +1,13 @@
 require 'spec_helper'
 
 describe UsersHelper do
-  describe "#find_geo_location" do
-    it "gets the geo location of the current user" do
-      helper.find_geo_location.should == {
-        :city         => '(Private Address)',
-        :region       => nil,
-        :country_code => 'XX',
-        :country      => nil,
-        :ip           => '0.0.0.0',
-        :timezone     => nil,
-      }
-    end
+  before do
+    helper.request.stub(:remote_ip).and_return('8.8.8.8')
+  end
 
-    it "gets the geo location for a given IP address" do
-      helper.find_geo_location('8.8.8.8').should == {
+  context "geo location" do
+    let(:geo_location_hash) do
+      {
         :city         => 'Mountain View',
         :region       => 'CA',
         :country_code => 'US',
@@ -24,6 +17,63 @@ describe UsersHelper do
         :ip           => '8.8.8.8',
         :timezone     => 'America/Los_Angeles',
       }
+    end
+
+    it "gets the geo location of the current user" do
+      helper.find_geo_location.should == geo_location_hash
+    end
+
+    context "geo location detectors" do
+      before do
+        helper.stub(:find_geo_location).and_return(geo_location_hash)
+      end
+
+      it "detects the country of the current user" do
+        helper.detected_country.should == 'United States'
+      end
+
+      it "detects the country code of the current user" do
+        helper.detected_country_code.should == 'US'
+      end
+
+      context "city and region" do
+        context "city with region" do
+          it "detects the city and region of the current user" do
+            helper.detected_city_and_region.should == 'Mountain View, CA'
+          end
+        end
+
+        context "city with no region" do
+          before { geo_location_hash[:region] = nil }
+
+          it "detects the city and region of the current user" do
+            helper.detected_city_and_region.should == 'Mountain View'
+          end
+        end
+
+        context "region with no city" do
+          before { geo_location_hash[:city] = nil }
+
+          it "detects the city and region of the current user" do
+            helper.detected_city_and_region.should == 'CA'
+          end
+        end
+
+        context "no city and no region" do
+          before do
+            geo_location_hash[:city]   = nil
+            geo_location_hash[:region] = nil
+          end
+
+          it "detects the city and region of the current user" do
+            helper.detected_city_and_region.should == ''
+          end
+        end
+      end
+
+      it "detects the timezone of the current user" do
+        helper.detected_timezone.should == 'America/Los_Angeles'
+      end
     end
   end
 end
