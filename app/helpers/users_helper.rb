907f52453e90ca4911bfa4f9df05e37872a7d816
Fred Wu <ifredwu@gmail.com>
2011-05-28 19:06:30 +1000

    find_geo_location.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def find_geo_location(remote_ip = nil)
+    GeoLocation.find(remote_ip || request.remote_ip)
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe UsersHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#find_geo_location" do
+    it "gets the geo location of the current user" do
+      helper.find_geo_location.should == {
+        :city         => '(Private Address)',
+        :region       => nil,
+        :country_code => 'XX',
+        :country      => nil,
+        :ip           => '0.0.0.0',
+        :timezone     => nil,
+      }
+    end
+
+    it "gets the geo location for a given IP address" do
+      helper.find_geo_location('8.8.8.8').should == {
+        :city         => 'Mountain View',
+        :region       => 'CA',
+        :country_code => 'US',
+        :country      => 'United States',
+        :longitude    => '-122.078',
+        :latitude     => '37.402',
+        :ip           => '8.8.8.8',
+        :timezone     => 'America/Los_Angeles',
+      }
+    end
+  end
 end
