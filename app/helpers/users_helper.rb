d723364ff340cb4179b5759c9f91d9f26a8b2b78
Fred Wu <ifredwu@gmail.com>
2011-05-29 01:26:09 +1000

    Cleaner code (and better implementation of 'detected_city_and_region').

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,31 +3,10 @@ module UsersHelper
     GeoLocation.find(request.remote_ip)
   end
 
-  def detected_country
-    find_geo_location[:country]
-  end
-
-  def detected_country_code
-    find_geo_location[:country_code]
-  end
-
-  def detected_city_and_region
-    if detected_city && detected_region
-      "#{detected_city}, #{detected_region}"
-    else
-      "#{detected_city}#{detected_region}"
-    end
-  end
-
-  def detected_city
-    find_geo_location[:city]
-  end
-
-  def detected_region
-    find_geo_location[:region]
-  end
-
-  def detected_timezone
-    find_geo_location[:timezone]
-  end
+  def detected_country;         find_geo_location[:country]                     end
+  def detected_country_code;    find_geo_location[:country_code]                end
+  def detected_city;            find_geo_location[:city]                        end
+  def detected_region;          find_geo_location[:region]                      end
+  def detected_timezone;        find_geo_location[:timezone]                    end
+  def detected_city_and_region; [detected_city, detected_region].compact * ', ' end
 end
