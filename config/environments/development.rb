000b3852e3ef79c9c793c96f381c676aed1a1849
Fred Wu <ifredwu@gmail.com>
2011-05-31 20:41:14 +1000

    Pretty HTML for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,6 @@ AngelNest::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  Slim::Engine.set_default_options :pretty => true
 end
