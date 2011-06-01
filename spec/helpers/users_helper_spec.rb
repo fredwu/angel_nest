b38a966488704240a987b9337f0997846eb768af
Fred Wu <ifredwu@gmail.com>
2011-06-02 08:16:52 +1000

    Removed a test for the external geo location service that requires a network connection.

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe UsersHelper do
-  before do
-    helper.request.stub(:remote_ip).and_return('8.8.8.8')
-  end
-
-  context "geo location" do
+  context "geo location detectors" do
     let(:geo_location_hash) do
       {
         :city         => 'Mountain View',
@@ -19,61 +15,55 @@ describe UsersHelper do
       }
     end
 
-    it "gets the geo location of the current user" do
-      helper.find_geo_location.should == geo_location_hash
+    before do
+      helper.stub(:find_geo_location).and_return(geo_location_hash)
     end
 
-    context "geo location detectors" do
-      before do
-        helper.stub(:find_geo_location).and_return(geo_location_hash)
-      end
+    it "detects the country of the current user" do
+      helper.detected_country.should == 'United States'
+    end
 
-      it "detects the country of the current user" do
-        helper.detected_country.should == 'United States'
-      end
+    it "detects the country code of the current user" do
+      helper.detected_country_code.should == 'US'
+    end
 
-      it "detects the country code of the current user" do
-        helper.detected_country_code.should == 'US'
-      end
-
-      context "city and region" do
-        context "city with region" do
-          it "detects the city and region of the current user" do
-            helper.detected_city_and_region.should == 'Mountain View, CA'
-          end
-        end
-
-        context "city with no region" do
-          before { geo_location_hash[:region] = nil }
-
-          it "detects the city and region of the current user" do
-            helper.detected_city_and_region.should == 'Mountain View'
-          end
-        end
-
-        context "region with no city" do
-          before { geo_location_hash[:city] = nil }
-
-          it "detects the city and region of the current user" do
-            helper.detected_city_and_region.should == 'CA'
-          end
-        end
-
-        context "no city and no region" do
-          before do
-            geo_location_hash[:city]   = nil
-            geo_location_hash[:region] = nil
-          end
-
-          it "detects the city and region of the current user" do
-            helper.detected_city_and_region.should == ''
-          end
+    context "city and region" do
+      context "city with region" do
+        it "detects the city and region of the current user" do
+          helper.detected_city_and_region.should == 'Mountain View, CA'
         end
       end
 
-      it "detects the timezone of the current user" do
-        helper.detected_timezone.should == 'America/Los_Angeles'
+      context "city with no region" do
+        before { geo_location_hash[:region] = nil }
+
+        it "detects the city and region of the current user" do
+          helper.detected_city_and_region.should == 'Mountain View'
+        end
       end
+
+      context "region with no city" do
+        before { geo_location_hash[:city] = nil }
+
+        it "detects the city and region of the current user" do
+          helper.detected_city_and_region.should == 'CA'
+        end
+      end
+
+      context "no city and no region" do
+        before do
+          geo_location_hash[:city]   = nil
+          geo_location_hash[:region] = nil
+        end
+
+        it "detects the city and region of the current user" do
+          helper.detected_city_and_region.should == ''
+        end
+      end
+    end
+
+    it "detects the timezone of the current user" do
+      helper.detected_timezone.should == 'America/Los_Angeles'
     end
   end
 end
