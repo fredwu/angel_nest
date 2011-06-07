547b999aa56951dc62923b44b4badb3b47cf5947
Fred Wu <ifredwu@gmail.com>
2011-06-07 18:59:51 +1000

    Basic layout!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def time_ago(time)
     t 'meta.time_ago', :time => time_ago_in_words(time)
   end
+
+  def link_to_current(*params)
+    link_to_unless_current(*params) do
+      link_to *params, :class => 'current'
+    end
+  end
 end
