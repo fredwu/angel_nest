51fa39e70a5452734fd3eb47f58bcd2206e2f420
Fred Wu <ifredwu@gmail.com>
2011-06-25 04:08:05 +1000

    Use asset pipeline to handle Modernizr.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require modernizr
 //= require jquery
 //= require jquery_ujs
 //= require rails.validations
