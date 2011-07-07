c8fdf7aeef7e39d3bb4cb3801a211624f7d9405e
Git Fly <gitfly@gmail.com>
2011-07-07 21:49:28 +0800

    Revert "Revert "Made the Devise's :login virtual attribute support more apparent.""
    This reverts commit 46f467db56d1802d290b8eb8895f26209a1d47f0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -140,47 +140,49 @@ class User < ActiveRecord::Base
 
   # Devise's support for login using the :login virtual attribute
   # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign_in-using-their-username-or-email-address
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login      = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  end
-
-  def self.send_reset_password_instructions(attributes={})
-    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-    recoverable.send_reset_password_instructions if recoverable.persisted?
-    recoverable
-  end
-
-  def self.find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
-    (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
-
-    attributes = attributes.slice(*required_attributes)
-    attributes.delete_if { |key, value| value.blank? }
-
-    if attributes.size == required_attributes.size
-      if attributes.has_key?(:login)
-         login = attributes.delete(:login)
-         record = find_record(login)
-      else
-        record = where(attributes).first
-      end
+  class << self
+    def find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      login      = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     end
 
-    unless record
-      record = new
-
-      required_attributes.each do |key|
-        value = attributes[key]
-        record.send("#{key}=", value)
-        record.errors.add(key, value.present? ? error : :blank)
-      end
+    def send_reset_password_instructions(attributes={})
+      recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+      recoverable.send_reset_password_instructions if recoverable.persisted?
+      recoverable
     end
-    record
-  end
 
-  def self.find_record(login)
-    where(["username = :value OR email = :value", { :value => login }]).first
+    def find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
+      (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
+
+      attributes = attributes.slice(*required_attributes)
+      attributes.delete_if { |key, value| value.blank? }
+
+      if attributes.size == required_attributes.size
+        if attributes.has_key?(:login)
+           login = attributes.delete(:login)
+           record = find_record(login)
+        else
+          record = where(attributes).first
+        end
+      end
+
+      unless record
+        record = new
+
+        required_attributes.each do |key|
+          value = attributes[key]
+          record.send("#{key}=", value)
+          record.errors.add(key, value.present? ? error : :blank)
+        end
+      end
+      record
+    end
+
+    def find_record(login)
+      where(["username = :value OR email = :value", { :value => login }]).first
+    end
   end
 
   private
